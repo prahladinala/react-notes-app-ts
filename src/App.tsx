0d957f4509c87e05ce31ec78d264f0f7736985bc
Prahlad <prahlad2018@gmail.com>
2023-08-11 10:29:59 +0530

    ✨ Added Comments to understand Routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ function App() {
   return (
     <Container>
       <Routes>
+        {/* HOME ROUTE - Default Route / View all Notes */}
         <Route path="/" element={<h1>Hi</h1>} />
+        {/* NEW ROUTE - Add new Notes */}
         <Route path="/new" element={<h1>New</h1>} />
+        {/* VIEW AND EDIT ROUTE - View and Edit Notes */}
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
         </Route>
+        {/* ANY OTHER ROUTE - Redirect to Home Page */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Container>
